755a7a38119ffa44e665842393002692ad1b9a69
Everth Paredes <epe0413@gmail.com>
2022-09-13 21:22:10 -0600

    Finalizando el Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,7 +36,7 @@
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Recordarme') }}</span>
                 </label>
             </div>
 
@@ -54,8 +54,8 @@
 
                 
             </div>
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
+            <x-primary-button class="w-full justify-center">
+                {{ __('Iniciar Sesión') }}
             </x-primary-button>
         </form>
     </x-auth-card>
