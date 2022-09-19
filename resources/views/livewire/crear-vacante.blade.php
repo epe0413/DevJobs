f8059fa8482e5fd39fb7d4982b94c19404e5bf29
Everth Paredes <epe0413@gmail.com>
2022-09-19 17:23:36 -0600

    Añadiendo los Campos

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -1,3 +1,60 @@
-<div>
-    <h1>Desde Livewire</h1>
-</div>
+<form class="md:w-1/2 space-y-3">
+    <div>
+        <x-input-label for="titulo" :value="__('Título Vacante')" />
+
+        <x-text-input id="titulo" class="block mt-1 w-full" type="text" name="titulo" :value="old('titulo')" placeholder="Título Vacante"/>
+    </div>
+
+    <div>
+        <x-input-label for="salario" :value="__('Salario Mensual')" />
+        <select
+            id="salario"
+            name="salario"
+            class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full"
+        >
+        </select>
+    </div>
+
+    <div>
+        <x-input-label for="categoria" :value="__('Categoría')" />
+        <select
+            id="categoria"
+            name="categoria"
+            class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full"
+        >
+        </select>
+    </div>
+
+    <div>
+        <x-input-label for="empresa" :value="__('Empresa')" />
+
+        <x-text-input id="empresa" class="block mt-1 w-full" type="text" name="empresa" :value="old('empresa')" placeholder="Empresa: ej. Netflix, Uber, Shopify"/>
+    </div>
+
+    <div>
+        <x-input-label for="ultimo_dia" :value="__('Último día para postularse')" />
+
+        <x-text-input id="ultimo_dia" class="block mt-1 w-full" type="date" name="ultimo_dia" :value="old('ultimo_dia')"/>
+    </div>
+
+    <div>
+        <x-input-label for="descripcion" :value="__('Descripción Puesto')" />
+
+        <textarea
+            name="descripcion"
+            placeholder="Descripción general del puesto, experiencia"
+            class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full h-72"
+        >
+        </textarea>
+    </div>
+
+    <div>
+        <x-input-label for="imagen" :value="__('Imagen')" />
+
+        <x-text-input id="imagen" class="block mt-1 w-full" type="file" name="imagen"/>
+    </div>
+
+    <x-primary-button>
+        Crear Vacante
+    </x-primary-button>
+</form>
