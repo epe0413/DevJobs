fcd0c82838a5e0a6005ee5a7ad7befce09e183a0
Everth Paredes <epe0413@gmail.com>
2022-09-20 21:06:28 -0600

    Añadiendo el Resto de Mensajes de Error

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -27,6 +27,10 @@
                 <option value="{{ $salario->id }}">{{ $salario->salario}}</option>
             @endforeach
         </select>
+        
+        @error('salario')
+            <livewire:mostrar-alerta :message="$message"/>
+        @enderror
     </div>
 
     <div>
@@ -41,18 +45,30 @@
                 <option value="{{ $categoria->id }}">{{ $categoria->categoria}}</option>
             @endforeach
         </select>
+
+        @error('categoria')
+            <livewire:mostrar-alerta :message="$message"/>
+        @enderror
     </div>
 
     <div>
         <x-input-label for="empresa" :value="__('Empresa')" />
 
         <x-text-input id="empresa" class="block mt-1 w-full" type="text" wire:model="empresa" :value="old('empresa')" placeholder="Empresa: ej. Netflix, Uber, Shopify"/>
+
+        @error('empresa')
+            <livewire:mostrar-alerta :message="$message"/>
+        @enderror
     </div>
 
     <div>
         <x-input-label for="ultimo_dia" :value="__('Último día para postularse')" />
 
         <x-text-input id="ultimo_dia" class="block mt-1 w-full" type="date" wire:model="ultimo_dia" :value="old('ultimo_dia')"/>
+
+        @error('ultimo_dia')
+            <livewire:mostrar-alerta :message="$message"/>
+        @enderror
     </div>
 
     <div>
@@ -64,12 +80,20 @@
             class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full h-40"
         >
         </textarea>
+        
+        @error('descripcion')
+            <livewire:mostrar-alerta :message="$message"/>
+        @enderror
     </div>
 
     <div>
         <x-input-label for="imagen" :value="__('Imagen')" />
 
         <x-text-input id="imagen" class="block mt-1 w-full" type="file" wire:model="imagen"/>
+
+        @error('imagen')
+            <livewire:mostrar-alerta :message="$message"/>
+        @enderror
     </div>
 
     <x-primary-button>
