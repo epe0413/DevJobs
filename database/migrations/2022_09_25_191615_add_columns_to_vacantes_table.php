a850455d329da31cd950176094b999cbf0073b8e
Everth Paredes <epe0413@gmail.com>
2022-09-25 14:05:03 -0600

    Solucionando problema del rollback

--- database/migrations/2022_09_25_191615_add_columns_to_vacantes_table.php
+++ database/migrations/2022_09_25_191615_add_columns_to_vacantes_table.php
@@ -34,6 +34,9 @@ return new class extends Migration
     public function down()
     {
         Schema::table('vacantes', function (Blueprint $table) {
+            $table->dropForeign('vacantes_categoria_id_foreign');
+            $table->dropForeign('vacantes_salario_id_foreign');
+            $table->dropForeign('vacantes_user_id_foreign');
             $table->dropColumn(['titulo','salario_id', 'categoria_id', 'empresa', 'ultimo_dia', 'descripcion', 'imagen', 'publicado', 'user_id']);
         });
     }
