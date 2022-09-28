6723404e645e06c0f5aa93b9d0b3f0e3e7678551
Everth Paredes <epe0413@gmail.com>
2022-09-28 15:42:42 -0600

    Mostrando información de la vacante

---
+++ resources/views/livewire/mostrar-vacante.blade.php
@@ -0,0 +1,22 @@
+<div class="p-8">
+    <div class="mb-3">
+        <h3 class="font-bold text-xl text-gray-800 my-2">
+            {{ $vacante->titulo }}
+        </h3>
+
+        <div class="md:grid md:grid-cols-2 bg-gray-50 p-4 my-3 rounded-md">
+            <p class="font-bold text-sm text-gray-800 my-1">Empresa: 
+                <span class="font-normal">{{ $vacante->empresa }}</span>
+            </p>
+            <p class="font-bold text-sm text-gray-800 my-1">Último día para postularse: 
+                <span class="font-normal">{{ $vacante->ultimo_dia->toFormattedDateString() }}</span>
+            </p>
+            <p class="font-bold text-sm text-gray-800 my-1">Categoria: 
+                <span class="font-normal">{{ $vacante->categoria_id }}</span>
+            </p>
+            <p class="font-bold text-sm text-gray-800 my-1">Salario: 
+                <span class="font-normal">{{ $vacante->salario_id }}</span>
+            </p>
+        </div>
+    </div>
+</div>
