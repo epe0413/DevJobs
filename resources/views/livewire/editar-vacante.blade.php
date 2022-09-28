85f6b753951281cb17f537cac5ac7d9d666e4459
Everth Paredes <epe0413@gmail.com>
2022-09-27 21:39:13 -0600

    Ajustando el Componente para edición

--- resources/views/livewire/editar-vacante.blade.php
+++ resources/views/livewire/editar-vacante.blade.php
@@ -1,4 +1,4 @@
-<form class="md:w-1/2 space-y-3" wire:submit.prevent='crearVacante'>
+<form class="md:w-1/2 space-y-3" wire:submit.prevent='editarVacante'>
     <div>
         <x-input-label for="titulo" :value="__('Título Vacante')" />
 
