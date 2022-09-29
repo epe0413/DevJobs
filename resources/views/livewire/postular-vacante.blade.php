a18f3293ed784a8bdee617d0b31356ac39ba55df
Everth Paredes <epe0413@gmail.com>
2022-09-29 16:34:51 -0600

    Añadir el HTML del Componente

--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <p>Aquí se mostrará un formulario</p>
+<div class="bg-gray-100 p-3 mt-3 flex flex-col justify-center items-center">
+    <h3 class="text-center text-xl font-bold my-2">Postularme a esta vacante</h3>
+
+    <form class="w-96 mt-3" action="">
+        <div class="mb-2">
+            <x-input-label for="cv" :value="__('Curriculum u hoja de vida')"/>
+            <x-text-input id="cv" type="file" accept=".pdf" class="block mt-1 w-full" />
+        </div>
+    </form>
 </div>
