9978c9d104253b66e9e46fbd963c60dfb02ae893
Everth Paredes <epe0413@gmail.com>
2022-09-30 12:00:02 -0600

    Agregando validación del Formulario

--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -1,11 +1,16 @@
 <div class="bg-gray-100 p-3 mt-3 flex flex-col justify-center items-center">
     <h3 class="text-center text-xl font-bold my-2">Postularme a esta vacante</h3>
 
-    <form class="w-96 mt-3" action="">
+    <form wire:submit.prevent='postularme' class="w-96 mt-3" action="">
         <div class="mb-2">
             <x-input-label for="cv" :value="__('Curriculum u hoja de vida')"/>
             <x-text-input id="cv" type="file" accept=".pdf" class="block mt-1 w-full" />
         </div>
+
+        @error('cv')
+            <livewire:mostrar-alerta :message="$message"/>
+        @enderror
+
         <x-primary-button class="my-3">
             {{ __('Postularme')}}
         </x-primary-button>
