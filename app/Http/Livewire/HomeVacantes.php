53342a9deb99718d7f6a3a2d1d003a08a07eff33
Everth Paredes <epe0413@gmail.com>
2022-10-05 13:56:40 -0600

    Leyendo los datos ingresados en el Formulario

--- app/Http/Livewire/HomeVacantes.php
+++ app/Http/Livewire/HomeVacantes.php
@@ -7,6 +7,19 @@ use Livewire\Component;
 
 class HomeVacantes extends Component
 {
+    public $termino;
+    public $salario;
+    public $categoria;
+
+    protected $listeners = ['terminosBusqueda'=> 'buscar'];
+
+    public function buscar($termino, $categoria, $salario)
+    {
+        $this->termino = $termino;
+        $this->categoria = $categoria;
+        $this->salario = $salario;        
+    }
+
     public function render()
     {
         $vacantes = Vacante::all();
